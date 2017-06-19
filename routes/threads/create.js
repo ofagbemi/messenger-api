33ff75faa55ec01f65b3bdd47225fd04acbfd4f1
Jare Fagbemi <ofagbemi@stanford.edu>
2017-06-19 03:46:18 -0700

    Add member_ids param to POST /thread

--- routes/threads/create.js
+++ routes/threads/create.js
@@ -1,3 +1,4 @@
+const { uniq } = require('ramda');
 const router = require('express').Router();
 
 const Thread = require('../../models/thread');
@@ -8,10 +9,10 @@ module.exports = router.post('/', authMiddleware, createThread);
 
 async function createThread(req, res, next) {
   const { id } = req.user;
-
+  const { member_ids: memberIds = [] } = req.body;
   try {
     const thread = await new Thread().save();
-    await thread.members().attach([id]);
+    await thread.members().attach(uniq([id].concat(memberIds)));
     return res.json(
       await new Thread({ id: thread.id }).fetch({ withRelated: 'members' })
     );
